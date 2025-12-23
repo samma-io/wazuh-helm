4cbd3fcd08fa9f51e51974e313356541e3b06a52
Miguel Elizalde <miguel.elizalde.19.98@live.com>
2025-12-22 19:25:28 -0700

    feat: Remove deprecated certificate and pause/start scripts for Wazuh

--- bin/make_certs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "Making certs for wazuh"
--- bin/start.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-echo "Set the wazuh on pause"
-kubectl patch statefulset wazuh-dashboard  -p '{"spec": {"replicas":1 }}' -n wazuh
-kubectl patch statefulset wazuh-indexer  -p '{"spec": {"replicas":1 }}' -n wazuh
-kubectl patch statefulset wazuh-manager-master  -p '{"spec": {"replicas":1 }}' -n wazuh
-kubectl patch statefulset wazuh-manager-worker-0  -p '{"spec": {"replicas":1 }}' -n wazuh
-
-
-
